6f535bb52bb26b8e7ab58e102f249415911eef6b
Raza <zuhairhasan2003@gmail.com>
2026-04-21 18:31:27 +0100

    exp of agressive cows problem

--- Agressive Cow Problem/main.cpp
+++ Agressive Cow Problem/main.cpp
@@ -3,6 +3,28 @@
 #include<algorithm>
 using namespace std;
 
+// PROBLEM EXPLANATION:
+/*
+    SETUP:
+    - You have a set of stalls (positions on a number line)
+    - You have a certain number of cows to place in these stalls
+
+    GOAL:
+    - Place the cows in the stalls so that the MINIMUM distance between any two cows is as LARGE as possible
+    
+    WHY?
+    - Cows fight when they are too close to each other
+    - So we want to spread them out as much as possible
+
+    EXAMPLE:
+    - Stalls are at positions : [1, 2, 4, 8, 9]
+    - Number of cows          : 3
+    
+    - We need to pick 3 positions out of [1, 2, 4, 8, 9]
+    - We want the chosen positions to be as far apart as possible
+    - The answer would be the LARGEST possible MINIMUM distance between any two cows
+*/
+
 bool IsValid(vector<int> & stalls, int numCows, int minDist)
 {
     int currCowIndex = 0;
